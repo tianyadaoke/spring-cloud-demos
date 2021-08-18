6e79719409d8cca5645c78f50b7de662e8100c20
Bing Zhou <bingfbi@gmail.com>
2021-08-19 00:05:22 +0200

    elastic search query

---
+++ hotel-es-demo/src/main/java/org/zb/hotelesdemo/HotelEsDemoApplication.java
@@ -0,0 +1,13 @@
+package org.zb.hotelesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelEsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelEsDemoApplication.class, args);
+    }
+
+}
---
+++ hotel-es-demo/src/test/java/org/zb/hotelesdemo/HotelEsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zb.hotelesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelEsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
